743487e73c98dfb7ceaa9a9834c76c8555dd17de
micheleq21 <ykiam4991@gmail.com>
2026-04-27 03:03:33 -0300

    Update alerta.py

--- api/alerta.py
+++ api/alerta.py
@@ -2,8 +2,8 @@ from http.server import BaseHTTPRequestHandler
 import json
 from supabase import create_client
 
-# Usa los datos que encontraste en Supabase (Settings -> API)
-URL = "https://knnnemdkahzovufelowc.supabase.co"
+# Pega aquí tus valores:
+URL = "https://knnnemdkahzovufelowc.supabase.co" 
 KEY = "sb_publishable_kCqiY8Y2T2JuDRMzYQsL8Q_KR7K-ee2" 
 
 supabase = create_client(URL, KEY)
@@ -14,7 +14,6 @@ class handler(BaseHTTPRequestHandler):
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data.decode('utf-8'))
         
-        # Guardar en Supabase
         try:
             supabase.table("alertas").insert({
                 "nombre": data.get("nombre"),
@@ -28,12 +27,12 @@ class handler(BaseHTTPRequestHandler):
             
             self.send_response(200)
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error al guardar: {e}")
             self.send_response(500)
             
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps({"status": "datos guardados"}).encode('utf-8'))
+        self.wfile.write(json.dumps({"status": "success"}).encode('utf-8'))
 
     def do_OPTIONS(self):
         self.send_response(200)
